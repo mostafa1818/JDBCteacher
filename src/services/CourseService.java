af2f3fc618a506bbf69222279db325b75948ecad
mostafa <mostafa18051b@yahoo.com>
2020-12-01 21:11:26 +0330

    first step for service

---
+++ src/services/CourseService.java
@@ -0,0 +1,111 @@
+package services;
+
+import objects.Course;
+import objects.Teacher;
+import util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseService {
+
+    public void save(Course course)
+    {
+        try(
+                Connection connection=ConnectionFactory.getconnection();
+                PreparedStatement preparedStatement=connection
+                        .prepareStatement("INSERT into Course (idteacher,idstudentId) values (?,?)");
+                )
+        {
+            preparedStatement.setInt(1,course.getTeacherId());
+            preparedStatement.setInt(2,course.getTeacherId());
+            preparedStatement.executeUpdate();
+        }catch (SQLException sqlException){sqlException.printStackTrace();}
+
+    }
+
+
+    public void edite(List<Course> course, int idteacher,int idstudent, int newidteacher,int newidstudent)
+    {
+        try(Connection connection= ConnectionFactory.getconnection();
+            PreparedStatement preparedStatement=connection.
+              prepareStatement
+                ("update  course set idteacher=?,idstudent=? where idteacher=? and where idstudent=?");
+        )
+        {
+            preparedStatement.setInt(1,newidteacher);
+            preparedStatement.setInt(2,newidstudent);
+            preparedStatement.setInt(3,idteacher);
+            preparedStatement.setInt(4,idstudent);
+            preparedStatement.executeUpdate();
+
+        }catch (SQLException sqlException){sqlException.printStackTrace();}
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void deleted(List<Course> course, int idteacher,int idstudent)
+    {
+
+        try(Connection connection= ConnectionFactory.getconnection();
+            PreparedStatement preparedStatement=connection.
+                    prepareStatement("delete from course where idteacher= ? and idstudent= ?");
+
+        )
+
+        {
+            preparedStatement.setInt(1,idteacher);
+            preparedStatement.setInt(1,idstudent);
+            preparedStatement.executeUpdate();
+        }catch (SQLException sqlException){sqlException.printStackTrace();}
+
+    }
+
+public List<Course> loadall()
+{
+    try(
+            Connection connection=ConnectionFactory.getconnection();
+            PreparedStatement preparedStatement=connection.prepareStatement("select * from Course");
+            ResultSet resultSet=preparedStatement.executeQuery();
+
+            )
+    {
+
+        List<Course> list=new ArrayList<>();
+        while(resultSet.next())
+        {
+            Course course=new Course();
+            course.setTeacherId(resultSet.getInt("idteacher"));
+            course.setStudentId(resultSet.getInt("idstudent"));
+            list.add(course);
+        }
+        return list;
+
+
+    }
+    catch (SQLException sqlException){sqlException.printStackTrace();}
+
+         return null;
+
+}
+
+
+}
---
+++ src/services/TeacherService.java
@@ -0,0 +1,94 @@
+package services;
+
+import objects.Teacher;
+import util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeacherService {
+
+    public void deleted(List<Teacher> teacher, int id)
+    {
+
+        try(Connection connection= ConnectionFactory.getconnection();
+            PreparedStatement preparedStatement=connection.
+                    prepareStatement("delete from teacher where idteacher= ?");
+
+        )
+
+        {
+            preparedStatement.setInt(1,id);
+
+            preparedStatement.executeUpdate();
+        }catch (SQLException sqlException){sqlException.printStackTrace();}
+
+    }
+
+    public void edite(List<Teacher> teacher, int id, String firstname, String LastName)
+    {
+        try(Connection connection= ConnectionFactory.getconnection();
+            PreparedStatement preparedStatement=connection.
+          prepareStatement("update  teacher set idteacher=?,FirstName=?,LastName=? where idteacher=?");
+        )
+        {
+            preparedStatement.setInt(1,id);
+            preparedStatement.setString(2,firstname);
+            preparedStatement.setString(3,LastName);
+            preparedStatement.setInt(4,id);
+            preparedStatement.executeUpdate();
+
+        }catch (SQLException sqlException){sqlException.printStackTrace();}
+
+    }
+
+
+    public void save(Teacher teacher)
+    {
+        try(Connection connection= ConnectionFactory.getconnection();
+            PreparedStatement preparedStatement=connection.
+                    prepareStatement("insert into Teacher(idteacher,FirstName, LastName) values(?,?,?)");
+        )
+        {
+            preparedStatement.setInt(1,teacher.getId());
+            preparedStatement.setString(2,teacher.getFirstName());
+            preparedStatement.setString(3,teacher.getLastName());
+            preparedStatement.executeUpdate();
+
+        }catch (SQLException sqlException){sqlException.printStackTrace();}
+
+    }
+    public List<Teacher> loadall()
+    {
+     try(
+             Connection connection=ConnectionFactory.getconnection();
+             PreparedStatement preparedStatement=connection.prepareStatement("select * from Teacher");
+             ResultSet resultSet=preparedStatement.executeQuery()
+             )
+     {
+         List<Teacher> list = new ArrayList<>();
+           while(resultSet.next())
+           {
+               Teacher teacher=new Teacher();
+               teacher.setId(resultSet.getInt("idteacher"));
+                teacher.setFirstName(resultSet.getString("FirstName"));
+                teacher.setLastName(resultSet.getString("LastName"));
+
+                   list.add(teacher);
+           }
+         return list;
+
+     }catch (SQLException sqlException){sqlException.printStackTrace();}
+
+        return null;
+    }
+
+
+
+
+
+}
