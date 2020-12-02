31aa246578aa79f2b57d247b8c3936d63fe27332
mostafa <mostafa18051b@yahoo.com>
2020-12-02 09:26:49 +0330

    ADD DataInputForEdite

--- src/datainput/InputDataForStudent.java
+++ src/datainput/InputDataForStudent.java
@@ -1,11 +1,31 @@
 package datainput;
 
 import objects.Student;
+import objects.Teacher;
 
 
 import java.util.Scanner;
 
 public class InputDataForStudent {
+
+
+
+
+    public Student DataInputForEdite()
+    {
+        Student student=new Student();
+        Scanner scan =new Scanner(System.in);
+        System.out.println("press id");
+        int intinput=scan.nextInt();
+        student.setId(intinput);
+        System.out.println("press firstname");
+        String input=scan.nextLine();
+        student.setFirstName(input);
+        System.out.println("press last");
+        input=scan.nextLine();
+        student.setLastName(input);
+        return student;
+    }
 
 
 
